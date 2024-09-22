4238301b82a0a30912be54a4e7f297e31242bc33
cristina-1 <montenegrocristinam@gmail.com>
2024-09-22 16:21:09 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import { Component } from 'react';
+import Productos from './components/carro-de-compras/Productos';    
+import Layout from './components/carro-de-compras/Layout'
+import Navbar from './components/carro-de-compras/Navbar'
+import Title from './components/carro-de-compras/Title'
+
+class App extends Component {
+   state = {
+     productos:[
+       {name: 'Tomate', price: 1500, img: '/Productos/tomate.png'},
+       {name: 'Arvejas', price: 2500, img: '/Productos/arvejas.png'},
+       {name: 'Lechuga', price: 500, img: '/Productos/lechuga.png'},
+    ],
+    carro: [],
+    
+  }
+    
+  agregarAlCarro = (producto) => {
+     const { carro} = this.state
+     if (carro.find(x => x.name === producto.name)) {
+       const newCarro = carro.map(x => x.name === producto.name
+       ? ({
+         ...x,
+         cantidad: x.cantidad + 1
+        })
+        : x) 
+      return this.setState({carro: newCarro})
+      }   
+       
+     return this.setState({
+       carro: this.state.carro.concat({
+        ...producto,
+         cantidad: 1,
+      })
+     })
+
+     
+  
+  }
+  
+  render() {
+    return (
+      <div>
+        <Navbar carro={this.state.carro}  />
+        <Layout>
+           <Title />
+           <Productos
+           agregarAlCarro={this.agregarAlCarro}
+           productos={this.state.productos}
+        /> 
+        </Layout>
+        
+      </div>
+    )
+  }
+}  
+
+  
+
+
+
+
+
+
+export default App 
+     
+       
+
+
+
+
+  
+
+     
+         
+       
+
+
+
+
+
+
---
+++ src/components/carro-de-compras/DetallesCarro.js
@@ -0,0 +1,25 @@
+import { Component } from "react";
+
+class DetallesCarro extends Component {
+    render() {
+    return (
+      <p>lala</p>
+    )   
+ }
+}
+
+export default DetallesCarro
+
+
+
+
+
+
+
+
+
+
+
+
+
+
